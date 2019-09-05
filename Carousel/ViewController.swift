4710c37beb94ccc3f6cc40f21e7952418a2ba2d7
Felipe Fernandes <vegan@govegans.it>
2019-09-05 11:52:11 +0200

    Initial Commit

---
+++ Carousel/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Carousel
+//
+//  Created by Felipe Fernandes on 05/09/2019.
+//  Copyright © 2019 Felipe Fernandes. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
